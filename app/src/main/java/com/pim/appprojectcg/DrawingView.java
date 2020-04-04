8117aa02a5a0dcb81add5938f18efa407f2526ce
seni <conta.jogos.i.r@gmail.com>
2020-04-04 10:02:26 +0100

    Drawing

---
+++ app/src/main/java/com/pim/appprojectcg/DrawingView.java
@@ -0,0 +1,70 @@
+package com.pim.appprojectcg;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.List;
+
+public class DrawingView extends View {
+
+    // setup initial color
+    private final int paintColor = Color.BLACK;
+    // defines paint and canvas
+    private Paint drawPaint;
+    // Store circles to draw each time the user touches down
+    private Path path = new Path();
+
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        setupPaint();
+    }
+
+    // Draw each circle onto the view
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPath(path, drawPaint);
+    }
+
+    // Get x and y and append them to the path
+    public boolean onTouchEvent(MotionEvent event) {
+        float pointX = event.getX();
+        float pointY = event.getY();
+        // Checks for the event that occurs
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                // Starts a new line in the path
+                path.moveTo(pointX, pointY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                // Draws line between last point and this point
+                path.lineTo(pointX, pointY);
+                break;
+            default:
+                return false;
+        }
+
+        postInvalidate(); // Indicate view should be redrawn
+        return true; // Indicate we've consumed the touch
+    }
+
+    // Setup paint with color and stroke styles
+    private void setupPaint() {
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(5);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+    }
+
+}
--- app/src/main/java/com/pim/appprojectcg/MainActivity.java
+++ app/src/main/java/com/pim/appprojectcg/MainActivity.java
@@ -2,13 +2,17 @@ package com.pim.appprojectcg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DrawingView drawView = findViewById(R.id.simpleDrawingView1);
     }
 }
